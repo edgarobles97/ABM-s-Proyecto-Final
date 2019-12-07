dd71fec1b236c26d884278eac606c0879fc4c73b
Edgar Robles <36207871+edgarobles97@users.noreply.github.com>
2019-12-06 20:58:52 -0600

    Add files via upload

--- TodosC.py
+++ TodosC.py
@@ -4,8 +4,10 @@ from mesa.time import RandomActivation
 from mesa.space import MultiGrid
 from mesa.datacollection import DataCollector
 
+from scipy.stats import poisson
+
 class miAgente(Agent): # Comenzamos definiendo a cada agente
-    def __init__(self, pos, model, gender, beauty, wealth, desired_beauty, desired_wealth):
+    def __init__(self, pos, model, gender, beauty, wealth, desired_beauty, desired_wealth, time_to_critical, sojourn, is_critical):
         super().__init__(pos, model) 
         self.pos = pos
         self.gender = gender
@@ -13,6 +15,10 @@ class miAgente(Agent): # Comenzamos definiendo a cada agente
         self.wealth = wealth
         self.desired_beauty = desired_beauty
         self.desired_wealth = desired_wealth 
+        
+        self.time_to_critical = time_to_critical
+        self.sojourn = -1
+        self.is_critical = 0
         
     def step(self): # Modelamos el movimiento
         vecindad = self.model.grid.get_neighborhood(self.pos,moore=True,include_center=False)
@@ -37,7 +43,22 @@ class miAgente(Agent): # Comenzamos definiendo a cada agente
                     self.model.hombres -= 1
                 else:
                     self.model.mujeres -= 1
-                
+        
+        unhappy = []
+        
+        self.sojourn += 1
+        if self.sojourn >= self.time_to_critical:
+            self.is_critical = 1
+            
+        if self.is_critical == 1:
+            self.model.schedule.remove(self)
+            self.model.grid.remove_agent(self)
+            unhappy.append(self)
+            if self.gender == 1:
+                    self.model.hombres -= 1
+            else:
+                    self.model.mujeres -= 1
+                  
 class TodosC(Model):
     '''
     Love-match market Model: 
@@ -45,7 +66,7 @@ class TodosC(Model):
     En este modelo, cada individuo recorre de manera aleatoria el lugar, al encontrarse con un match (agente del sexo opuesto con parámetros de belleza y riqueza coincidentes con lo deseado) desaparece del modelo. 
     El objetivo es observar la distribución de perfiles de belleza y riqueza a lo largo del tiempo hasta ver quienes no logran encontrar pareja. 
     '''
-    def __init__(self, height=50, width=50, density=0.8, HM_pc=0.2, entry_rate=30): # Aquí establecemos el tamaño del Grid donde se desarrolla el modelo, además de los parámetros iniciales.
+    def __init__(self, height=50, width=50, density=0.8, HM_pc=0.2, entry_rate=1, max_agents=750): # Aquí establecemos el tamaño del Grid donde se desarrolla el modelo, además de los parámetros iniciales.
         self.height = height
         self.width = width
         self.density = density
@@ -54,7 +75,7 @@ class TodosC(Model):
             
         self.schedule = RandomActivation(self)
         self.grid = MultiGrid(height, width, torus=False)
-            
+        self.max_agents = max_agents
         self.parejas = 0
         self.hombres = 0
         self.mujeres = 0
@@ -75,10 +96,10 @@ class TodosC(Model):
                 else:
                     gender = 0
                     self.mujeres += 1
-                agent = miAgente((x, y), self, gender, beauty = random.uniform(0,1), wealth = random.uniform(0,1), desired_beauty = random.uniform(0,1), desired_wealth = random.uniform(0,1))
+                agent = miAgente((x, y), self, gender, beauty = random.gauss(3,5), wealth = random.gauss(2,6), desired_beauty = random.gauss(2,5), desired_wealth = random.gauss(3,3), time_to_critical = random.gauss(20, 5), sojourn = -1, is_critical= 0)
                 self.grid.place_agent(agent, (x, y))
                 self.schedule.add(agent)
-
+        
         self.running = True
         self.datacollector.collect(self)
 
@@ -87,5 +108,23 @@ class TodosC(Model):
         # Por fines gráficos, recolectamos la información sobre la cantidad de parejas
         self.datacollector.collect(self)
 
-        if self.schedule.get_agent_count() == 0 or self.hombres == 0 or self.mujeres == 0: 
+        if self.schedule.get_agent_count() == 0: 
             self.running = False
+    
+    def update(self, entry_fct= lambda x: poisson.rvs(x)):
+        if self.schedule.get_agent_count()<self.max_agents:
+            new_agents = entry_fct(self.entry_rate) 
+            for cell in self.grid.coord_iter():
+                x = cell[1]
+                y = cell[2]
+                if self.random.random() < self.density:
+                    if self.random.random() < self.HM_pc:
+                        gender = 1 
+                        self.hombres += 1
+                    else:
+                        gender = 0
+                        self.mujeres += 1
+            for new_agent in new_agents:
+                new_agent = miAgente((x, y), self, gender, beauty = random.gauss(0,1), wealth = random.gauss(0,1), desired_beauty = random.gauss(0,1), desired_wealth = random.gauss(0,1))
+                new_agent.grid.place_agent(new_agent, (x,y))
+        
