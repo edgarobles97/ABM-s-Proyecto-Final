62a1b951f4f996daa8cc593bc7704bda9596bb73
DanJB96 <58237841+DanJB96@users.noreply.github.com>
2019-12-10 14:58:37 -0600

    Update LoveMatch.py

--- LoveMatch.py
+++ LoveMatch.py
@@ -14,7 +14,6 @@ class miAgente(Agent): # Comenzamos definiendo a cada agente
         self.wealth = wealth
         self.desired_beauty = desired_beauty
         self.desired_wealth = desired_wealth 
-        
         self.time_to_critical = time_to_critical
         self.sojourn = -1
         self.is_critical = 0
@@ -30,32 +29,28 @@ class miAgente(Agent): # Comenzamos definiendo a cada agente
         if len(matches)==1:  # Si encuentra pareja, desaparecen del grid
             self.model.schedule.remove(self)
             self.model.grid.remove_agent(self)
-            if self.gender == 1:
-                self.model.hombres -= 1
-            else:
-                self.model.mujeres -= 1
             self.model.parejas += 1 # Contabilizamos una pareja en la lista para futura recolección
             for m in matches: 
                 self.model.schedule.remove(m)
                 self.model.grid.remove_agent(m) 
-                if m.gender == 1:
-                    self.model.hombres -= 1
-                else:
-                    self.model.mujeres -= 1
-        
+
         self.sojourn += 1
         if self.sojourn >= self.time_to_critical:
             self.is_critical = 1
             
         if self.is_critical == 1:
+            self.model.unhappy_agents.append(self) 
             self.model.schedule.remove(self)
             self.model.grid.remove_agent(self)
             self.model.unhappy += 1
-            if self.gender == 1:
-                    self.model.hombres -= 1
-            else:
-                    self.model.mujeres -= 1
-                  
+            
+    def promedio_beauty_unhappy(model):
+        sum_beauty = 0
+        for a in model.schedule.agents:
+            sum_beauty += a.beauty
+        prom_beauty = sum_beauty/len(model.unhappy_agents)
+        return prom_beauty
+    
 class LoveMatch(Model):
     '''
     Love-match market Model: 
@@ -63,24 +58,23 @@ class LoveMatch(Model):
     En este modelo, cada individuo recorre de manera aleatoria el lugar, al encontrarse con un match (agente del sexo opuesto con parámetros de belleza y riqueza coincidentes con lo deseado) desaparece del modelo. 
     El objetivo es observar la distribución de perfiles de belleza y riqueza a lo largo del tiempo hasta ver quienes no logran encontrar pareja. 
     '''
-    def __init__(self, height=50, width=50, density=0.8, HM_pc=0.2, entry_rate=1, max_agents=750): # Aquí establecemos el tamaño del Grid donde se desarrolla el modelo, además de los parámetros iniciales.
+    def __init__(self, height=50, width=50, density=0.8, HM_pc=0.2, entry_rate=5, max_agents=250): # Aquí establecemos el tamaño del Grid donde se desarrolla el modelo, además de los parámetros iniciales.
         self.height = height
         self.width = width
         self.density = density
         self.HM_pc = HM_pc
-        
+        self.dummy = 0
         self.entry_rate = 5
             
         self.schedule = RandomActivation(self)
         self.grid = MultiGrid(height, width, torus=False)
         self.max_agents = max_agents
         self.parejas = 0
-        self.hombres = 0
-        self.mujeres = 0
         self.unhappy = 0
-        self.datacollector = DataCollector(
-        {"parejas": "parejas", "unhappy": "unhappy"},
-        {"x": lambda a: a.pos[0], "y": lambda a: a.pos[1]})
+        self.unhappy_agents = []
+        max_agents = height*width
+        
+
             
 # En esta sección, etiquetamos a cada agente según su tipo 
 
@@ -90,51 +84,65 @@ class LoveMatch(Model):
             if self.random.random() < self.density:
                 if self.random.random() < self.HM_pc:
                     gender = 1 
-                    self.hombres += 1
+                    
                 else:
                     gender = 0
-                    self.mujeres += 1
-                agent = miAgente((x, y), self, gender, beauty = random.gauss(3,5),
-                                                         wealth = random.gauss(2,6),
-                                                         desired_beauty = random.gauss(2,5), 
-                                                         desired_wealth = random.gauss(3,3),
-                                                         time_to_critical = random.gauss(20, 5), 
+                   
+                agent = miAgente((x, y), self, gender, beauty = random.gauss(0.5,0.15),
+                                                         wealth = random.gauss(0.5,0.15),
+                                                         desired_beauty = random.gauss(0.5,0.1), 
+                                                         desired_wealth = random.gauss(0.5,0.1),
+                                                         time_to_critical = random.gauss(20,5), 
                                                          sojourn = -1,
-                                                         is_critical= 0)   
+                                                         is_critical= 0)     
                 self.schedule.add(agent)
                 self.grid.place_agent(agent, (x,y))
         
         self.running = True
+        self.datacollector = DataCollector(agent_reporters ={"wealth":"wealth"} )
         self.datacollector.collect(self)
+                            
         
-                
-
-    def update(self):
-        if self.schedule.get_agent_count()<self.max_agents:
+    def step(self):# Este step permite que el modelo siga corriendo hasta que todos los agentes tengan pareja
+        self.schedule.step()
+        if self.schedule.get_agent_count() < (self.max_agents-self.entry_rate):
+            for cell in self.grid.coord_iter():
+                n_x = cell[1]
+                n_y = cell[2]
             for i in range(self.entry_rate):
-                x = self.random.randrange(self.grid.width)
-                y = self.random.randrange(self.grid.height)
-                if self.random.random() < self.HM_pc:
-                    gender = 1 
-                    self.hombres += 1
-                else:
-                    gender = 0
-                    self.mujeres += 1
+                new_beauty = random.gauss(0.5,0.15)
+                new_wealth = random.gauss(0.5,0.15)
+                new_desired_beauty = random.gauss(0.5, 0.15)
+                new_desired_wealth = random.gauss(0.5, 0.15)
+                new_time_to_critical = random.gauss(20,5)
+                new_sojourn = -1
+                new_is_critical = 0
 
-                agent = miAgente(i, self,gender, 
-                                 beauty = random.gauss(4,2),
-                                 wealth = random.gauss(4,3), 
-                                 desired_beauty = random.gauss(4,3),
-                                 desired_wealth = random.gauss(3,2),
-                                 time_to_critical = random.gauss(20,5),
-                                 sojourn = -1,
-                                 is_critical = 0)
-                self.schedule.add(agent)
-                self.grid.place_agent(agent, (x,y))
-    def step(self): # Este step permite que el modelo siga corriendo hasta que todos los agentes tengan pareja
-        self.schedule.step()    
+                if self.random.random() < self.HM_pc:
+                    new_gender = 1 
+                    
+                else:
+                    new_gender = 0
+                
+            new_agent = miAgente((n_x,n_y), i, 
+                                 gender = new_gender, 
+                                 beauty = new_beauty,
+                                 wealth = new_wealth, 
+                                 desired_beauty = new_desired_beauty,
+                                 desired_wealth = new_desired_wealth,
+                                 time_to_critical = new_time_to_critical,
+                                 sojourn = new_sojourn,
+                                 is_critical = new_is_critical )
+
+            self.schedule.add(new_agent)
+            self.grid.place_agent(new_agent, (n_x,n_y))
+    
+            self.dummy = self.schedule.get_agent_count()
+            print(str(self.dummy))
+
         # Por fines gráficos, recolectamos la información sobre la cantidad de parejas
         self.datacollector.collect(self)
-
+        
+        self.datacollector.get_agent_vars_dataframe().to_csv("dc.csv")
         if self.schedule.get_agent_count() == 0: 
             self.running = False
